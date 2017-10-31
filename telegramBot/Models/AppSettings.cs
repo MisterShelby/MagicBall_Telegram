76e37cb23558ff374a3a0ce3c1ea0d11c3440d10
Ivan <solvan007@gmail.com>
2017-10-31 15:27:24 +0200

    Update AppSettings.cs

--- telegramBot/Models/AppSettings.cs
+++ telegramBot/Models/AppSettings.cs
@@ -4,7 +4,7 @@ namespace telegramBot.Models
 {
     public class AppSettings
     {
-        public static string Url { get; } = "https://magicball.ga:443/{0}";
+        public static string Url { get; } = "your url}";
 
         public static string Key { get;  } = "your API code";
     }
